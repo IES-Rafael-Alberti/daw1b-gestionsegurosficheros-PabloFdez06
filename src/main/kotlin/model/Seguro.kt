cec139045820f16270eade9e556f3fa8939cda27
PabloFdez06 <pablofernandez.nytelweb@outlook.es>
2025-03-27 12:56:38 +0100

    Parte model terminada, falta colocar los requires en cuanto resuelva una duda, y comenzamos data.

--- src/main/kotlin/model/Seguro.kt
+++ src/main/kotlin/model/Seguro.kt
@@ -6,15 +6,13 @@ abstract class Seguro(
     var importe: Double
 ) : IExportable {
 
-    companion object {
-        var numPolizasAuto = 400000
-        var numPolizasHogar = 100000
-        var numPolizasVida = 800000
-    }
+
 
     abstract fun calcularImporteAnioSiguiente(interes: Double): Double
 
-    abstract fun tipoSeguro(): String
+    fun tipoSeguro(): String {
+        return this::class.simpleName ?: "Desconocido"
+    }
 
     override fun serializar(separador: String): String {
         return "$numPoliza$separador$dniTitular$separador$importe"
@@ -25,13 +23,12 @@ abstract class Seguro(
     }
 
     override fun hashCode(): Int {
-        return numPoliza
+        return numPoliza.hashCode()
     }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other == null || javaClass != other.javaClass) return false
-        val seguro = other as Seguro
-        return numPoliza == seguro.numPoliza
+        if (other !is Seguro) return false
+        return numPoliza == other.numPoliza
     }
 }
--- src/main/kotlin/model/SeguroHogar.kt
+++ src/main/kotlin/model/SeguroHogar.kt
@@ -15,32 +15,31 @@ class SeguroHogar: Seguro {
         const val CICLO_ANIOS_INCREMENTO = 5
         var numPolizasHogar: Int = 100000
 
-        fun obtenerNumPolizaHogar(): Int {
-            return numPolizasHogar++
-        }
 
-        fun crearSeguro(datos: List<String>) : SeguroHogar?{
 
-            try { // controlar fuera, donde pedimos el seguro.
-                if (datos.size == 7) {
-                    datos[0].toInt()
-                    datos[1].isNotEmpty()
-                    datos[2].toDouble()
-                    datos[3].toInt()
-                    datos[4].toInt()
-                    datos[5].isNotEmpty()
-                    LocalDate.parse(datos[6])
-                }
-            } catch (e: IllegalArgumentException) {
-                return null
+        fun crearSeguro(datos: List<String>) : SeguroHogar? {
+            return try {
+                if (datos.size < 7) return null
+
+                val numPoliza = datos[0].toInt()
+                val dniTitular = datos[1]
+                val importe = datos[2].toDouble()
+                val metrosCuadrados = datos[3].toInt()
+                val valorContenido = datos[4].toInt()
+                val direccion = datos[5]
+                val anioConstruccion = LocalDate.parse(datos[6])
+
+
+                SeguroHogar(numPoliza, dniTitular, importe, metrosCuadrados, valorContenido, direccion, anioConstruccion)
+            } catch (e: Exception) {
+                null
             }
-            return // TODO
         }
     }
 
 
     constructor(dniTitular: String, importe: Double, metrosCuadrados: Int, valorContenido: Int, direccion: String, anioConstruccion: LocalDate) :
-            super(numPoliza = obtenerNumPolizaHogar(), dniTitular, importe) {
+            super(numPoliza = numPolizasHogar++, dniTitular, importe) {
                 this.metrosCuadrados = metrosCuadrados
                 this.valorContenido = valorContenido
                 this.direccion = direccion
@@ -57,14 +56,18 @@ class SeguroHogar: Seguro {
     }
 
     override fun calcularImporteAnioSiguiente(interes: Double): Double {
-        TODO("Not yet implemented")
+        val aniosAntiguedad = LocalDate.now().year - anioConstruccion.year
+        val incrementoAdicional = (aniosAntiguedad / CICLO_ANIOS_INCREMENTO) * PORCENTAJE_INCREMENTO_ANIOS
+        return importe * (1 + interes + incrementoAdicional)
     }
 
-    override fun tipoSeguro(): String {
-        return this::class.simpleName ?: "Desconocido"
-    }
 
     override fun serializar(separador: String): String {
         return super.serializar(separador) + "$metrosCuadrados$separador$valorContenido$separador$direccion$separador$anioConstruccion$"
     }
+
+    override fun toString(): String {
+        return "Seguro Hogar(numPoliza= ${super.numPoliza}, dniTitular= ${super.dniTitular}, importe= ${"%.2f".format(super.importe)}, metrosCuadrados= $metrosCuadrados, valorContenido= $valorContenido, direccion= $direccion, anioConstruccion= $anioConstruccion)"
+    }
+
 }
